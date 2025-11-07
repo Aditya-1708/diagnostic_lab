63083873045449a84458219fea05cb5f030a210e
aditya umesh <umeshaditya72@gmail.com>
2025-11-07 14:55:26 +0530

    rmooved

--- client/src/pages/DeliveryPickup.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DeliveryPickup() {
-  return (
-    <div>DeliveryPickup</div>
-  )
-}
-
-export default DeliveryPickup
--- client/src/pages/DoctorsConsultation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DoctorsConsultation() {
-  return (
-    <div>DoctorsConsultation</div>
-  )
-}
-
-export default DoctorsConsultation
